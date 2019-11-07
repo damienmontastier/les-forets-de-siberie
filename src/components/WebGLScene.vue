551b62add0a07cac303641d0edc3d10638147726
ROCHE Clement <croche@g-a102a-08.local>
2019-11-07 10:57:08 +0100

    init

---
+++ src/components/WebGLScene.vue
@@ -0,0 +1,24 @@
+<template>
+  <canvas class="webGLScene"></canvas>
+</template>
+
+<script>
+import Vue from 'vue'
+import WebGL from '@/webGL'
+export default {
+  name: 'WebglScene',
+  created() {},
+  mounted() {
+    // Init webgl
+
+    this.$nextTick(() => {
+      Vue.prototype.$webgl = Vue.prototype.$webgl || new WebGL(this.$el)
+      this.$webgl.init()
+    })
+  },
+  beforeDestroy() {
+    this.$webgl.destroy()
+    Vue.prototype.$webgl = null
+  }
+}
+</script>
