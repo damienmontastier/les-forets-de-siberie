04033156f655aa77827ca2eccdb8166a89155d6b
Damien Montastier <33467958+damienmontastier@users.noreply.github.com>
2019-11-27 19:07:04 +0100

    wip

--- src/webGL/utils/Parallax.js
+++ src/webGL/utils/Parallax.js
@@ -10,9 +10,10 @@ export default class Parallax {
   init() {
     VirtualScroll.on(e => {
       Object.values(this.layers.children).forEach((element, index) => {
-        const speed = index
+        const speed = index == 0 ? 1 * 0.09 : index * 0.09
+        console.log(speed)
         TweenMax.to(element.position, 0.8, {
-          y: '-=' + e.deltaY / speed,
+          y: '+=' + e.deltaY * speed,
           ease: Power4.easeOut,
         })
       })
