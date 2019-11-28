02ad37a84e19ad569e923b3116a970ecda8edde5
Damien Montastier <33467958+damienmontastier@users.noreply.github.com>
2019-11-28 12:27:13 +0100

    wip

--- src/webGL/stages/Avril.js
+++ src/webGL/stages/Avril.js
@@ -3,11 +3,12 @@ import gsap from 'gsap'
 import Layers from '@/webGL/utils/Layers'
 import Parallax from '@/webGL/utils/Parallax'
 import loadSeveralTextureAtlasFromPathes from '@/webGL/utils/loadSeveralTextureAtlasFromPathes'
-import Parts from '@/webGL/utils/Parts'
+import Part from '@/webGL/utils/Part'
 
 const pathesArray = [
   '/assets/avril/atlases/part1/',
   '/assets/avril/atlases/part2/',
+  '/assets/avril/atlases/part3/',
 ]
 
 class Chapter1 extends THREE.Object3D {
@@ -16,10 +17,11 @@ class Chapter1 extends THREE.Object3D {
   }
   init() {
     this.loadAssets().then(textures => {
-      console.log(textures)
+      this.textures = textures
+
+      this.initParts()
 
       //Create part
-
       // const layers = new Layers({
       //   textures: this.textures,
       //   textureAtlas: this.textureAtlas,
@@ -34,6 +36,47 @@ class Chapter1 extends THREE.Object3D {
         resolve(textures)
       })
     })
+  }
+  initParts() {
+    this.parts = {}
+    for (let [name, layers] of Object.entries(this.partedTextures)) {
+      this.parts[name] = new Part({ name, layers })
+    }
+    //TODO add this.parts to the scene
+    // console.log(this.parts)
+  }
+  get utilsTextures() {
+    let textures = {}
+    Object.keys(this.textures)
+      .filter(texture => texture.includes('utils'))
+      .forEach(key => {
+        textures[key] = this.textures[key]
+      })
+    return textures
+  }
+  get partedTextures() {
+    let parts = {}
+
+    let textures = {}
+    Object.keys(this.textures)
+      .filter(texture => !texture.includes('utils'))
+      .forEach(key => {
+        textures[key] = this.textures[key]
+      })
+
+    for (let [name, texture] of Object.entries(textures)) {
+      let partIndex = name
+        .split('_')
+        .filter(index => index.includes('part'))[0]
+        .replace('part', '')
+      let layerIndex = name
+        .split('_')
+        .filter(index => index.includes('layer'))[0]
+        .replace('layer', '')
+      if (!parts['part' + partIndex]) parts['part' + partIndex] = {}
+      parts['part' + partIndex]['layer' + layerIndex] = texture
+    }
+    return parts
   }
 }
 
